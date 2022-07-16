53e7c7615faa504ba4f18556ea903274b468855a
szh881 <77442268+szh881@users.noreply.github.com>
2022-07-16 15:12:21 +0800

    Add files via upload

---
+++ week3_BufferPoolManager/sunzhihao/lru_replacer.h
@@ -0,0 +1,57 @@
+//===----------------------------------------------------------------------===//
+//
+//                         BusTub
+//
+// lru_replacer.h
+//
+// Identification: src/include/buffer/lru_replacer.h
+//
+// Copyright (c) 2015-2021, Carnegie Mellon University Database Group
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include <list>
+#include <mutex>  // NOLINT
+#include <unordered_map>
+#include <vector>
+
+#include "buffer/replacer.h"
+#include "common/config.h"
+
+namespace bustub {
+
+    /**
+     * LRUReplacer implements the Least Recently Used replacement policy.
+     */
+    class LRUReplacer : public Replacer {
+    public:
+        /**
+         * Create a new LRUReplacer.
+         * @param num_pages the maximum number of pages the LRUReplacer will be required to store
+         */
+        explicit LRUReplacer(size_t num_pages);
+
+        /**
+         * Destroys the LRUReplacer.
+         */
+        ~LRUReplacer() override;
+
+        bool Victim(frame_id_t* frame_id) override;
+
+        void Pin(frame_id_t frame_id) override;
+
+        void Unpin(frame_id_t frame_id) override;
+
+        size_t Size() override;
+
+    private:
+        // TODO(student): implement me!
+        std::mutex mutex_;
+        std::list<frame_id_t> lru_list_;                                            // 双向链表，存放frame_id_t
+        std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> lru_hash_;  // 哈希表，value存储一个迭代器
+        size_t max_size_;                                                           // 最大容量
+    };
+
+}  // namespace bustub
